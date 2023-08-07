167e2238b758342c00313a4864202bbae4bdc9ca
Marshall Williams <marshall@mwthink.net>
2023-08-07 16:08:02 -0500

    added prometheus + http server for scraping

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,11 @@ export default {
     application_id: process.env['DISCORD_APPLICATION_ID'] as string,
     public_key: process.env['DISCORD_PUBLIC_KEY'] as string,
     token: process.env['DISCORD_TOKEN'] as string,
-  }
+  },
+  http: {
+    port: Number(process.env['HTTP_PORT'] || 3000),
+  },
+  metrics: {
+    enable_default: Boolean(process.env['METRICS_ENABLE_DEFAULT'])
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,38 @@ import { REST } from 'discord.js';
 import { discordRegisterCommands } from './lib';
 import config from './config';
 import commands from './commands';
+import * as Http from 'http';
+import * as PromClient from 'prom-client';
+
+if(config.metrics.enable_default){
+  PromClient.collectDefaultMetrics()
+}
+
+// Create HTTP server
+const server = Http.createServer(async (req, res) => {
+  if(req.url == '/favicon.ico'){
+    res.writeHead(404);
+    return res.end();
+  }
+  else if(req.url == '/healthz'){
+    res.writeHead(200);
+    return res.end('ok');
+  }
+  else if(req.url == '/metrics'){
+    try {
+      const metrics = await PromClient.register.metrics();
+      res.writeHead(200);
+      return res.end(metrics);
+    }
+    catch(err){
+      console.error(err);
+      res.writeHead(500);
+      return res.end('An unexpected error has occurred.');
+    }
+  }
+  res.writeHead(404);
+  return res.end('404 - not found');
+})
 
 // Create Discord REST client
 const discordRest = new REST({
@@ -29,13 +61,30 @@ client.on('interactionCreate', async interaction => {
   interactionCommand.handler(interaction);
 })
 
-// Start the Discord client
-client.login(config.discord.token);
+server.listen(config.http.port, () => {
+  console.log('Server listening on port', config.http.port);
+  // Start the Discord client
+  client.login(config.discord.token);
+})
 
 // Handle graceful shutdown
 const shutdown = async () => {
   console.log('Application is shutting down');
-  await client.destroy()
+  try {
+    console.log('Stopping discord client');
+    await client.destroy()
+  }
+  catch(err){
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('Stopping HTTP service');
+  server.close((err) => {
+    if(err){
+      console.error(err);
+      process.exit(1);
+    }
+  })
 }
 
 ['SIGINT', 'SIGTERM'].forEach(sig => process.on(sig, () => {
